02c45441cf8a6be39103bfbdcee203dc24300c2d
namyoungjun96 <ehdgornltls@naver.com>
2022-01-04 23:39:23 +0900

    2022.01.04 codingtest

--- python/programmers/diskcontroller3.py
+++ python/programmers/diskcontroller3.py
@@ -1,13 +1,31 @@
 # 프로그래머스 우선순위 큐 3단계 디스크 컨트롤러
-# 아직 못품
+# 30분 생각 컷
 
 import heapq
 
 def solution(jobs):
     answer = 0
+    divide = len(jobs)
+    jobs.sort(reverse = True)
+    time = 0
+    heap = []
     
+    while jobs or heap:
+        if jobs and time >= jobs[len(jobs)-1][0]:
+            heapq.heappush(heap, [jobs[len(jobs)-1][1], jobs[len(jobs)-1][0]])
+            jobs.pop()
+        elif heap:
+            time += heap[0][0]
+            answer += time - heap[0][1]
+            heapq.heappop(heap)
+        else:
+            print("insert time")
+            time = jobs[len(jobs)-1][0]
+    
+    answer //= divide
+    print(answer)
     return answer
 
-jobs = [[0, 3], [1, 9], [2, 6]]
+jobs = [[0, 3], [1, 9], [2, 6], [20, 3]]
 solution(jobs)
 # 9
