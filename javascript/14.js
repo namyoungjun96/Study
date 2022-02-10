0393cbd263b4ed67b784ae7506df9b6ff0a2f65f
namyoungjun96 <ehdgornltls@naver.com>
2022-02-11 01:40:43 +0900

    2022.02.11 javascript study

---
+++ javascript/14.js
@@ -0,0 +1,18 @@
+var Counter = (function () {
+  var num = 0;
+
+  return {
+    increase() {
+      return ++num;
+    },
+    decrease() {
+      return --num;
+    },
+  };
+})();
+
+console.log(Counter.num);
+console.log(Counter.increase());
+console.log(Counter.increase());
+console.log(Counter.decrease());
+console.log(Counter.decrease());
---
+++ javascript/15.js
@@ -0,0 +1,20 @@
+// // 브라우저에서 실행해야 한다. var로 선언한 전역변수(객체),  전역객체 window의 프로퍼티다.
+// var x = 1;
+// y = 2;
+
+// function foo() {}
+
+// console.log(window.x);
+// console.log(x);
+
+// console.log(window.y);
+// console.log(y);
+
+// console.log(window.foo);
+// console.log(foo);
+
+// // 그러나 let, const로 선언한 전역변수는 전역객체 window의 프로퍼티가 아니다.
+// let x = 1;
+
+// console.log(window.x);
+// console.log(x);
